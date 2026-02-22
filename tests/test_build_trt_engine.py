7e9942b7e0b41432e44c06ec312ab22b7b50b8f0
thinksyncs <develop@toppymicros.com>
2026-02-22 17:09:54 +0900

    Test: cover trtexec flag detection

--- tests/test_build_trt_engine.py
+++ tests/test_build_trt_engine.py
@@ -25,6 +25,9 @@ class _DummyNetwork:
 
 
 class TestBuildTrtEngine(unittest.TestCase):
+    def setUp(self):
+        build_trt_engine._TRTEXEC_HELP_CACHE.clear()
+
     def test_resolve_input_name_prefers_requested(self):
         net = _DummyNetwork(["images", "other"])
         name = build_trt_engine._resolve_input_name(net, "images")
@@ -39,6 +42,26 @@ class TestBuildTrtEngine(unittest.TestCase):
         net = _DummyNetwork([])
         name = build_trt_engine._resolve_input_name(net, "images")
         self.assertEqual(name, "images")
+
+    def test_trtexec_workspace_flag_prefers_mem_pool(self):
+        build_trt_engine._TRTEXEC_HELP_CACHE["trtexec"] = " --memPoolSize=workspace:4096 "
+        arg = build_trt_engine._trtexec_workspace_arg("trtexec", 4096)
+        self.assertEqual(arg, "--memPoolSize=workspace:4096")
+
+    def test_trtexec_workspace_flag_prefers_legacy_workspace(self):
+        build_trt_engine._TRTEXEC_HELP_CACHE["trtexec"] = " --workspace=4096 "
+        arg = build_trt_engine._trtexec_workspace_arg("trtexec", 4096)
+        self.assertEqual(arg, "--workspace=4096")
+
+    def test_trtexec_timing_cache_flag_is_optional(self):
+        build_trt_engine._TRTEXEC_HELP_CACHE["trtexec"] = ""
+        arg = build_trt_engine._trtexec_timing_cache_arg("trtexec", Path("/tmp/timing.cache"))
+        self.assertIsNone(arg)
+
+    def test_trtexec_timing_cache_flag_supported(self):
+        build_trt_engine._TRTEXEC_HELP_CACHE["trtexec"] = " --timingCacheFile=/tmp/timing.cache "
+        arg = build_trt_engine._trtexec_timing_cache_arg("trtexec", Path("/tmp/timing.cache"))
+        self.assertEqual(arg, "--timingCacheFile=/tmp/timing.cache")
 
 
 if __name__ == "__main__":
