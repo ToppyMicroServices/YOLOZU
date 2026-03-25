128ecaf3dfc46d8c658f91a6a0f2f53887c8476f
thinksyncs <develop@toppymicros.com>
2026-03-25 21:24:48 +0900

    Reduce additional CodeQL backlog across runtime helpers

--- rtdetr_pose/rtdetr_pose/rtdetr_pose/optim_factory.py
+++ rtdetr_pose/rtdetr_pose/rtdetr_pose/optim_factory.py
@@ -1,4 +1,9 @@
 from __future__ import annotations
 
-from ..optim_factory import *  # noqa: F403
+from .. import optim_factory as _optim_factory
 
+is_norm_layer = _optim_factory.is_norm_layer
+build_param_groups = _optim_factory.build_param_groups
+build_optimizer = _optim_factory.build_optimizer
+
+__all__ = ["is_norm_layer", "build_param_groups", "build_optimizer"]
